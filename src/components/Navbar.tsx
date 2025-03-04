1cae12ab3cecf6c19f8ab530ff104039dae8bf04
Mario <omega398@live.de>
2025-03-04 21:51:31 +0100

    Responsive Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,17 @@
+import MobileNavbar from './MobileNavbar'
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+        <div className='h-24 flex items-center justify-between'>
+          {/* Links */ }
+          <div className="font-bold text-xl text-blue-600">Social Network</div>
+          {/* Mitte */ }
+          <div className="hidden"></div>
+          {/* Rechts */ }
+          <div className="">
+              <MobileNavbar />
+          </div>
+        </div>
   )
 }
 
